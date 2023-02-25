e2f82b4df6522888d809632163cadb2f43036c9a
allanschuch <allan20.1@hotmail.com>
2023-02-25 19:02:57 -0300

    Co-authored-by: Rafael Grimmler da Rocha
    <RafaelGrimmler@users.noreply.github.com>

--- src/AnaliseLexica.java
+++ src/AnaliseLexica.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
-enum TokenType{ NUM,SOMA, MULT,APar,FPar, EOF}
+enum TokenType{ NUM,SOMA,MULT,SUB,DIV,APar,FPar,EOF}
 
 class Token{
-  char lexema;
+  String lexema;
   TokenType token;
 
- Token (char l, TokenType t)
+ Token (String l, TokenType t)
  	{ lexema=l;token = t;}	
 
 }
@@ -27,37 +27,50 @@ class AnaliseLexica {
 		Token token;
 		int eof = -1;
 		char currchar;
+		String naturalNumberString = "";
+		boolean nextIsNumber = false;
 		int currchar1;
 
 			do{
-				currchar1 =  arquivo.read();
+				currchar1 = arquivo.read();  
 				currchar = (char) currchar1;
-			} while (currchar == '\n' || currchar == ' ' || currchar =='\t' || currchar == '\r');
-			
+				arquivo.mark(1);
+				char nextChar = (char) arquivo.read();
+				if(nextChar >= '0' && nextChar <= '9' && currchar >= '0' && currchar <= '9') nextIsNumber = true;
+				else nextIsNumber = false;
+				arquivo.reset();
+				if(currchar >= '0' && currchar <= '9') naturalNumberString = naturalNumberString + currchar;
+			} while (currchar == '\n' || currchar == ' ' || currchar =='\t' || currchar == '\r' || nextIsNumber);
+
 			if(currchar1 != eof && currchar1 !=10)
 			{
-								
+							
 	
 				if (currchar >= '0' && currchar <= '9')
-					return (new Token (currchar, TokenType.NUM));
+					return (new Token (naturalNumberString, TokenType.NUM));
+				
 				else
 					switch (currchar){
 						case '(':
-							return (new Token (currchar,TokenType.APar));
+							return (new Token ("" + currchar,TokenType.APar));
 						case ')':
-							return (new Token (currchar,TokenType.FPar));
+							return (new Token ("" + currchar,TokenType.FPar));
 						case '+':
-							return (new Token (currchar,TokenType.SOMA));
+							return (new Token ("" + currchar,TokenType.SOMA));
 						case '*':
-							return (new Token (currchar,TokenType.MULT));
-						
+							return (new Token ("" + currchar,TokenType.MULT));
+						case '/':
+							return (new Token ("" + currchar,TokenType.DIV));
+						case '-':
+							return (new Token ("" + currchar,TokenType.SUB));
+							
 						default: throw (new Exception("Caractere inválido: " + ((int) currchar)));
 					}
 			}
 
 			arquivo.close();
-			
-		return (new Token(currchar,TokenType.EOF));
+
+		return (new Token("" + currchar, TokenType.EOF));
 		
 	}
 }
